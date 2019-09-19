4893050a1a304c88a8f13c09b87aa942a485e3c7
Lucas Rabelo Moreaux <1601076@SALAS.AULAS>
2019-09-19 20:55:08 -0300

    retirando nome

--- com/moreaux/test/test_operacoes.py
+++ com/moreaux/test/test_operacoes.py
@@ -7,5 +7,5 @@ class TestOperacoes(TestCase):
     def setUp(self):
         self.operacoes = Operacoes()
 
-    def test_soma(self):
+    def test_velocidade(self):
         self.assertEqual(self.operacoes.velocidade([100,20]),"Should be velocidade: 5 m/s")
